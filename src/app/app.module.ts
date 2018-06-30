381dcb3a7dc556897e9aeb6576176f8769976b06
Alumina <origamijr@sbcglobal.net>
2018-06-30 01:40:24 -0700

    Started putting stuff in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 
 @NgModule({
   declarations: [
