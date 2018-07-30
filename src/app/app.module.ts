83fc3b64110fe7abd528404a16d75e329c86a993
Alumina <origamijr@sbcglobal.net>
2018-07-30 01:24:29 -0700

    starting to refactor into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { MarkdownModule } from 'ngx-markdown';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { MarkdownWrapperComponent } from './common/markdown-wrapper/markdown-wrapper.component';
-import { AnimateFieldComponent } from './common/animate/animate-field/animate-field.component';
-import { AnimateObjComponent } from './common/animate/animate-obj/animate-obj.component';
+import { MarkdownWrapperComponent } from './common/components/markdown-wrapper/markdown-wrapper.component';
+import { AnimateFieldComponent } from './common/components/animate/animate-field/animate-field.component';
+import { AnimateObjComponent } from './common/components/animate/animate-obj/animate-obj.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,8 @@ import { AnimateObjComponent } from './common/animate/animate-obj/animate-obj.co
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    AppRoutingModule,
     MarkdownModule.forRoot({ loader: HttpClient }),
     HttpClientModule
   ],
